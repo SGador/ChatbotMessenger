b3bf31d2f299bce1107532e8246b95c8b63a154d
SGador <30827601+SGador@users.noreply.github.com>
2017-12-14 20:18:00 +0800

    2nd Commit

--- app.js
+++ app.js
@@ -1,35 +1,109 @@
 
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
 var app = express();
+var mongoose = require('mongoose');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+//[SH] Bring in the data model
+require('./models/db');
+console.log('models loaded');
+// [SH] Bring in the Passport config after model is defined
+require('./config/passport');
+console.log('passport config loaded');
+//[SH] Initialise Passport before using the route middleware
+app.use(passport.initialize());
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', __dirname + '/app_client/views');
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(express.static(path.join(__dirname, 'app_client')));
+app.use(express.static(path.join(__dirname, 'app_client','app')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use('/boi', express.static(path.join(__dirname,'app_client','controllers')));
+app.use('/pic', express.static(path.join(__dirname,'app_client','images')));
+app.use('/service',express.static(path.join(__dirname,'app_client','services')));
 
+require('./routes')(app);
+console.log('routes loaded');
+//sdsdsfsdfsd
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+}
+//error handlers
+//catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+//[SH] Catch unauthorised errors
+app.use(function (err, req, res, next) {
+if (err.name === 'UnauthorizedError') {
+ res.status(401);
+ res.json({"message" : err.name + ": " + err.message});
+}
+});
+//development error handler
+//will print stacktrace
+if (app.get('env') === 'development') {
+ app.use(function(err, req, res, next) {
+     res.status(err.status || 500);
+     res.render('error', {
+         message: err.message,
+         error: err
+     });
+ });
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+//production error handler
+//no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+ res.status(err.status || 500);
+ res.render('error', {
+     message: err.message,
+     error: {}
+ });
 });
+
+//botkit
+require('dotenv').load();
+
+var middleware = require('botkit-middleware-watson')({
+  username: process.env.CONVERSATION_USERNAME,
+  password: process.env.CONVERSATION_PASSWORD,
+  workspace_id: process.env.WORKSPACE_ID,
+  url: process.env.CONVERSATION_URL || 'https://gateway.watsonplatform.net/conversation/api',
+  version_date: '2017-05-26'
+});
+
+module.exports = function(app) {
+  if (process.env.USE_FACEBOOK) {
+    var Facebook = require('./bot-facebook');
+    Facebook.controller.middleware.receive.use(middleware.receive);
+    Facebook.controller.createWebhookEndpoints(app, Facebook.bot);
+    console.log('Facebook bot is live');
+  }
+  // Customize your Watson Middleware object's before and after callbacks.
+  middleware.before = function(message, conversationPayload, callback) {
+    callback(null, conversationPayload);
+  }
+
+  middleware.after = function(message, conversationResponse, callback) {
+    callback(null, conversationResponse);
+  }
+};
+
+//http.createServer(app).listen(app.get('port'), function(){
+//  console.log('Express server listening on port ' + app.get('port'));
+//});
