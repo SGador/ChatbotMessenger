e156efc04eb43a9dbfdf20686ffcab10a5c3f79a
SGador <30827601+SGador@users.noreply.github.com>
2018-02-06 13:37:30 +0800

    added export

--- bot-facebook.js
+++ bot-facebook.js
@@ -130,3 +130,4 @@ controller.on('facebook_postback', function(bot, message){
 module.exports.controller = controller;
 module.exports.bot = bot;
 module.exports.endConversation = endConversation;
+module.exports.processWatsonResponse = processWatsonResponse;
