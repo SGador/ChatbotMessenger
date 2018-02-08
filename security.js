1d5b5633142da3c3ffc8544f9769a9908f0c3cbb
SGador <30827601+SGador@users.noreply.github.com>
2018-02-08 16:03:52 +0800

    revised more logging

--- security.js
+++ security.js
@@ -2,14 +2,13 @@ var crypto = require('crypto');
 
 module.exports = function verifyFacebookSignatureHeader(req, res, buf) {
   var signature = req.headers["x-hub-signature"];
-  console.log(req);
+  console.log(JSON.stringify(req));
   console.log(signature);
   if (!signature) {
     console.log("Signature absent in the request: %s", JSON.stringify(req.origin));
   } else {
     // Get the facebook signature
-	console.log(signature);
-    var elements = signature.split('sha1=');
+	var elements = signature.split('sha1=');
     var facebookSignature = elements[1];
 
     var expectedSignature = crypto.createHmac('sha1', process.env.FB_APP_SECRET)
