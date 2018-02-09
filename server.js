9e2c5dcc32170857a6b4e36b58d5cc7fac7e296e
SGador <30827601+SGador@users.noreply.github.com>
2018-02-09 19:45:35 +0800

    test

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.post('/update',function(req,res){
 	var test = {};
 	test.req = req;
 	test.res = res;
-	app(test);
+	app(req,res);
     //Facebook.processWatsonResponse(Facebook.bot, msg);
     res.send('200');
 });
