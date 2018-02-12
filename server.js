3e3fdd4e9c6b1281684c412fddd1cb34ef90fd4b
SGador <30827601+SGador@users.noreply.github.com>
2018-02-12 15:59:55 +0800

    test

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.listen(port, function() {
   console.log('Client server listening on port ' + port);
 });
 
-app.post('/update',function(req,res){
+/*app.post('/update',function(req,res){
 	console.log(JSON.stringify(req.body));
 	var userid = req.body.psid;
 	var Facebook = require('./bot-facebook');
@@ -40,4 +40,44 @@ app.post('/update',function(req,res){
 	app((req,res));
     //Facebook.processWatsonResponse(Facebook.bot, msg);
     res.send('200');
-});
+});*/
+
+app.post('/update', function (req, res) {
+
+	  console.log("$$$$$$$$$$$$$$$$$$$ Received from Client Portal" + JSON.stringify(req.body) + "$$$$$$$$$$$$$$$$$$$");
+	  var userid = req.body.userId;
+	  var Facebook = require('./bot-facebook');
+	  if (Facebook.bot == null)
+	    console.log("Facebook bot is null");
+	 
+	  if (Facebook.watsonMiddleware == null)
+	    console.log("Facebook bot is null");
+	  logs.view('log_user', 'by_userid', {
+	    key: [userid], include_docs: true
+	  }, function (err, res) {
+	    if (!err) {
+	     
+	      if (res.rows.length != 0) {
+	        console.log("Go here" + res.rows.length);
+	        function sortByTimestamp(a,b)
+	        {
+	          return ((a.value.message.timestamp < b.value.message.timestamp) ? 1 : ((a.value.message.timestamp > b.value.message.timestamp) ? -1 : 0));
+	        }
+	        var sortedArray = (res.rows).sort(sortByTimestamp);
+	       
+	        var msg = sortedArray[0].value.message;
+	        console.log("The text is "+ msg.text +"The timestamp is "+ msg.timestamp);
+	        msg.text = 'received from client portal';
+	        
+	        
+	          Facebook.processWatsonResponse(Facebook.bot, msg);
+	      }
+	    }
+	    else {
+	      console.log(err);
+	    }
+
+	  });
+	  res.set('X-Frame-Options','ALLOW-FROM https://kariteun-shopping.mybluemix.net/');
+	  res.send('hello')));
+	});
