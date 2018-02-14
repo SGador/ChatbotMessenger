5abbe833e3792497544529742c96e3f79c9eeb75
SGador <30827601+SGador@users.noreply.github.com>
2018-02-14 19:11:00 +0800

    test3

--- bot-facebook.js
+++ bot-facebook.js
@@ -65,7 +65,7 @@ function endConversation(message){
 }
 
 var processWatsonResponse = function(bot, message){
-	console.log('bot-facebook');
+	/*console.log('bot-facebook');*/
   console.log("Just heard the following message: " + JSON.stringify(message));
   if(message.watsonError){
     console.log("Watson Error: " + JSON.stringify(message.watsonError));
