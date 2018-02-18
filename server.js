0a823781dee25adc6e233a6e0f233e596336d9fa
SGador <stevengador@gmail.com>
2018-02-18 21:41:24 +0800

    test

--- server.js
+++ server.js
@@ -35,12 +35,12 @@ app.post('/update', function (req, res) {
 	  }
 	  var msg = {};
 	  var text;
-	  var orderData = (";" + req.body.orderData.orderId + ";" + req.body.orderData.address + ";" + req.body.orderData.totalPrice);
+	  var orderData = req.body.orderData.orderId + ";" + req.body.orderData.address + ";" + req.body.orderData.totalPrice;
 	  console.log(orderData);
 	  if (req.body.text == "ADDTOCART"){
 		  text = "<watson> add to cart";
 	  }else if(req.body.text == "CHECKOUT"){
-		  text = "<watson> transaction confirmation" + orderData;
+		  text = "<watson> transaction confirmation;" + orderData;
 	  }
 	  msg = {"text":text,"channel":userid,"user":userid,"timestamp":ts,"orderdata":req.body.orderData};
 
